8529c3b18605d8cdd21370c6f710a8c09231f99e
albertadevor <alberta.devor@gmail.com>
2017-04-15 17:24:49 -0400

    Setting up some ch-ch-ch-checkstyle

--- src/main/java/edu/brown/cs/courseler/repl/Main.java
+++ src/main/java/edu/brown/cs/courseler/repl/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import spark.ExceptionHandler;
@@ -12,7 +13,6 @@ import spark.Request;
 import spark.Response;
 import spark.Spark;
 import spark.template.freemarker.FreeMarkerEngine;
-import freemarker.template.Configuration;
 
 /**
  * The Main class of our project. This is where execution begins.
@@ -20,83 +20,81 @@ import freemarker.template.Configuration;
  * @author jj
  */
 public final class Main {
-	private static final int DEFAULT_PORT = 4567;
+  private static final int DEFAULT_PORT = 4567;
 
-	/**
-	 * The initial method called when execution begins.
-	 *
-	 * @param args
-	 *            An array of command line arguments
-	 */
-	public static void main(String[] args) {
-		new Main(args).run();
-	}
+  /**
+   * The initial method called when execution begins.
+   *
+   * @param args
+   *          An array of command line arguments
+   */
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
 
-	private String[] args;
+  private String[] args;
 
-	private Main(String[] args) {
+  private Main(String[] args) {
 
-		this.args = args;
-	}
+    this.args = args;
+  }
 
-	private void run() {
-		// Parse command line arguments
-		OptionParser parser = new OptionParser();
-		parser.accepts("gui");
-		parser.accepts("port").withRequiredArg().ofType(Integer.class)
-		.defaultsTo(DEFAULT_PORT);
-		OptionSet options = parser.parse(args);
+  private void run() {
+    // Parse command line arguments
+    OptionParser parser = new OptionParser();
+    parser.accepts("gui");
+    parser.accepts("port").withRequiredArg().ofType(Integer.class)
+        .defaultsTo(DEFAULT_PORT);
+    OptionSet options = parser.parse(args);
 
-		if (options.has("gui")) {
-			runSparkServer((int) options.valueOf("port"));
-		}
+    if (options.has("gui")) {
+      runSparkServer((int) options.valueOf("port"));
+    }
 
-	}
+  }
 
-	private static FreeMarkerEngine createEngine() {
-		Configuration config = new Configuration();
-		File templates = new File(
-				"src/main/resources/spark/template/freemarker");
-		try {
-			config.setDirectoryForTemplateLoading(templates);
-		} catch (IOException ioe) {
-			System.out.printf("ERROR: Unable use %s for template loading.%n",
-					templates);
-			System.exit(1);
-		}
-		return new FreeMarkerEngine(config);
-	}
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("ERROR: Unable use %s for template loading.%n",
+          templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
 
-	private void runSparkServer(int port) {
+  private void runSparkServer(int port) {
 
-		Spark.port(port);
-		Spark.externalStaticFileLocation("src/main/resources/static");
-		Spark.exception(Exception.class, new ExceptionPrinter());
+    Spark.port(port);
+    Spark.externalStaticFileLocation("src/main/resources/static");
+    Spark.exception(Exception.class, new ExceptionPrinter());
 
-		FreeMarkerEngine freeMarker = createEngine();
+    FreeMarkerEngine freeMarker = createEngine();
 
-		// Setup Spark Routes
-	}
+    // Setup Spark Routes
+  }
 
-	/**
-	 * Display an error page when an exception occurs in the server.
-	 *
-	 * @author jj
-	 */
-	private static class ExceptionPrinter implements ExceptionHandler {
-		@Override
-		public void handle(Exception e, Request req, Response res) {
-			res.status(500);
+  /**
+   * Display an error page when an exception occurs in the server.
+   *
+   * @author jj
+   */
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(500);
 
-			StringWriter stacktrace = new StringWriter();
-			try (PrintWriter pw = new PrintWriter(stacktrace)) {
-				pw.println("<pre>");
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
 
-				e.printStackTrace(pw);
-				pw.println("</pre>");
-			}
-			res.body(stacktrace.toString());
-		}
-	}
-
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
+  }
 }
