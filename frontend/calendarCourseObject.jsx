aface06da1d22c83f1194aa68f6d893adf085b2c
kokonakajima <koko_nakajima@brown.edu>
2017-04-17 14:42:30 -0400

    started calendar frontend

---
+++ frontend/calendarCourseObject.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import api from './api.jsx';
+
+export default class CalendarCourseObject extends React.Component {
+
+
+	render() {
+	return (
+	  <div className='calendarCourseObject'>
+	    <h3>here is a course object</h3>
+	  </div>
+	)
+	}
+}
