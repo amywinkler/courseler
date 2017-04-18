3b92629d795c790cf06768bec4090d4dd6160e34
kokonakajima <koko_nakajima@brown.edu>
2017-04-18 00:52:21 -0400

    preferences page is now less janky

--- frontend/preferencesScreen.jsx
+++ frontend/preferencesScreen.jsx
@@ -3,7 +3,86 @@ import ReactDOM from 'react-dom';
 import api from './api.jsx';
 
 export default class PreferencesScreen extends React.Component {
-  render() {
-    return <h1>user preferences go here! <a href='#' onClick={() => this.props.onDone()}>done</a></h1>
+
+  constructor(props) {
+     super(props);
+     this.state = {
+       classYear: '',
+       concentration: '',
+       favoriteClass: '',
+       departmentalInterests: ''
+     };
+     this.displayExistingPreferences();
   }
+
+  render() {
+
+  	let classYearField = <input type='text' value={this.state.classYear} onChange={ (e) => {this.setState({classYear: e.target.value})} } />;
+    let concentrationField = <input type='text' value={this.state.concentration} onChange={ (e) => {this.setState({concentration: e.target.value})} } />;
+    let favoriteClassField = <input type='text' value={this.state.favoriteClass} onChange={ (e) => {this.setState({favoriteClass: e.target.value})} } />;
+    let departmentalInterestsField = <input type='text' value={this.state.departmentalInterests} onChange={ (e) => {this.setState({departmentalInterests: e.target.value})} } />;
+
+    let doneButton = <a href='#' onClick={() => this.done()}>done</a>;
+
+    return (
+    	<div className='Preferences'>
+	    	<h1>User Preferences! </h1>
+	    	<div>
+	    		<label>Class Year</label>
+	    		{classYearField}
+	    	</div>
+	    	<div>
+	    		<label>Concentration</label>
+	    		{concentrationField}
+	    	</div>
+	    	<div>
+	    		<label>Favorite Class</label>
+	    		{favoriteClassField}
+	    	</div>
+	    	<div>
+	    		<label>Departmental Interests</label>
+	    		{departmentalInterestsField}
+	    	</div>
+    		{doneButton}
+    	</div>
+    )
+  }
+
+  /*
+		Called when user hits the 'done' button.
+		Sends data to backend, and executes onDone
+  */
+  done() {
+  	this.post(); 
+  	this.props.onDone();
+  }
+
+  /*
+		Sends preferences to backend
+  */
+  post() {
+  	let prefs = {
+  		class_year: this.state.classYear,
+    	concentration: this.state.concentration,
+    	favorite_class: this.state.favoriteClass,
+    	dept_interests: this.state.departmentalInterests
+  	};
+  	api.postPrefs(prefs);
+  }
+
+
+  /*
+  	Displays any existing user preferences using the api call.
+  */
+  displayExistingPreferences() {
+  	let display = (preferences) => {
+    	this.setState({classYear: preferences.class_year});
+    	this.setState({concentration: preferences.concentration});
+    	this.setState({favoriteClass: preferences.favorite_class});
+    	this.setState({departmentalInterests: preferences.dept_interests});
+    }
+  	api.getPrefs(display);
+  }
+
+
 }
