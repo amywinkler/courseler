dce0f8439ac1af6c923186d5ee97a9c0a477e17b
albertadevor <alberta.devor@gmail.com>
2017-04-17 20:13:40 -0400

    Refactored main.java so it's teeny tiny! @nate+koko front end and all get/post request can now be found in courseler.api.RequestHandler.java

--- src/main/java/edu/brown/cs/courseler/api/RequestHandler.java
+++ src/main/java/edu/brown/cs/courseler/api/RequestHandler.java
@@ -1,11 +1,101 @@
 package edu.brown.cs.courseler.api;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+
+import com.google.gson.Gson;
+
+import freemarker.template.Configuration;
+import spark.ExceptionHandler;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+import spark.TemplateViewRoute;
+import spark.template.freemarker.FreeMarkerEngine;
+
 /**
- * The main request handler class where all API calls can be called.
- * 
+ * The main request handler class where all API calls can be called. All calls
+ * from GUI come in from here.
+ *
  * @author adevor
  *
  */
-public class RequestHandler {
+public final class RequestHandler {
+
+  private static final Gson GSON = new Gson();
+
+  private RequestHandler() {
+
+  }
+
+  /**
+   * Run the spark server!
+   *
+   * @param port
+   *          the port it runs at.
+   */
+  public static void runSparkServer(int port) {
+
+    Spark.port(port);
+    Spark.externalStaticFileLocation("src/main/resources/static");
+    Spark.exception(Exception.class, new ExceptionPrinter());
+
+    FreeMarkerEngine freeMarker = createEngine();
+
+    // Setup Spark Routes:
+    Spark.get("/", new MainHandler(), freeMarker);
+  }
+
+  /**
+   * Main homepage.
+   *
+   * @author nateparrott
+   *
+   */
+  private static final class MainHandler implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      HashMap<String, Object> variables = new HashMap<String, Object>();
+      return new ModelAndView(variables, "main.ftl");
+    }
+  }
+
+  /**
+   * Display an error page when an exception occurs in the server.
+   *
+   * @author jj
+   */
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(500);
+
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
+  }
+
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("ERROR: Unable use %s for template loading.%n",
+          templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
 
 }
--- src/main/java/edu/brown/cs/courseler/repl/Main.java
+++ src/main/java/edu/brown/cs/courseler/repl/Main.java
@@ -1,22 +1,8 @@
 package edu.brown.cs.courseler.repl;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.HashMap;
-
+import edu.brown.cs.courseler.api.RequestHandler;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
-import spark.ExceptionHandler;
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
-import spark.Spark;
-import spark.TemplateViewRoute;
-import spark.template.freemarker.FreeMarkerEngine;
-import edu.brown.cs.courseler.data.CourseDataParser;
-import freemarker.template.Configuration;
 
 /**
  * The Main class of our project. This is where execution begins.
@@ -47,72 +33,11 @@ public final class Main {
     OptionParser parser = new OptionParser();
     parser.accepts("gui");
     parser.accepts("port").withRequiredArg().ofType(Integer.class)
-    .defaultsTo(DEFAULT_PORT);
+        .defaultsTo(DEFAULT_PORT);
     OptionSet options = parser.parse(args);
 
     if (options.has("gui")) {
-      runSparkServer((int) options.valueOf("port"));
-    }
-
-  }
-
-  private static FreeMarkerEngine createEngine() {
-    Configuration config = new Configuration();
-    File templates = new File("src/main/resources/spark/template/freemarker");
-    try {
-      config.setDirectoryForTemplateLoading(templates);
-    } catch (IOException ioe) {
-      System.out.printf("ERROR: Unable use %s for template loading.%n",
-          templates);
-      System.exit(1);
-    }
-    return new FreeMarkerEngine(config);
-  }
-
-  private void runSparkServer(int port) {
-
-    Spark.port(port);
-    Spark.externalStaticFileLocation("src/main/resources/static");
-    Spark.exception(Exception.class, new ExceptionPrinter());
-
-    FreeMarkerEngine freeMarker = createEngine();
-
-    // Setup Spark Routes:
-    Spark.get("/", new MainHandler(), freeMarker);
-  }
-
-  /**
-   * Main homepage.
-   *
-   * @author nateparrott
-   *
-   */
-  private static final class MainHandler implements TemplateViewRoute {
-    @Override
-    public ModelAndView handle(Request req, Response res) {
-      HashMap<String, Object> variables = new HashMap<String, Object>();
-      return new ModelAndView(variables, "main.ftl");
-    }
-  }
-
-  /**
-   * Display an error page when an exception occurs in the server.
-   *
-   * @author jj
-   */
-  private static class ExceptionPrinter implements ExceptionHandler {
-    @Override
-    public void handle(Exception e, Request req, Response res) {
-      res.status(500);
-
-      StringWriter stacktrace = new StringWriter();
-      try (PrintWriter pw = new PrintWriter(stacktrace)) {
-        pw.println("<pre>");
-
-        e.printStackTrace(pw);
-        pw.println("</pre>");
-      }
-      res.body(stacktrace.toString());
+      RequestHandler.runSparkServer((int) options.valueOf("port"));
     }
   }
 }
