1679b3197ce62e92e4a7342aed23f66034fff286
kokonakajima <koko_nakajima@brown.edu>
2017-04-25 03:25:20 -0400

    multiple concentrations / dept interests

--- frontend/preferencesScreen.jsx
+++ frontend/preferencesScreen.jsx
@@ -3,15 +3,43 @@ import ReactDOM from 'react-dom';
 import api from './api.jsx';
 import { currentRoute, navigateToRoute } from './routing.jsx';
 
+class ConcentrationField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      concentration: ''
+    }
+  }
+  componentWillReceiveProps() {
+    this.setState({concentration: this.props.concentrations[this.props.index]});
+  }
+  render() {
+    return (
+      <select name="concentration" 
+          value={this.state.concentration} 
+          onChange={ (e) => {
+            this.changeConcentration(e);
+          } }> 
+        {this.props.departments}
+      </select>
+    )
+  }
+  changeConcentration(e) {
+    this.setState({concentration: [e.target.value]});
+    this.props.onchange(e.target.value, this.props.index);
+  }
+}
+
+
 export default class PreferencesScreen extends React.Component {
 
   constructor(props) {
      super(props);
      this.state = {
        classYear: '',
-       concentration: '',
-       departmentalInterests: '',
-       departments: [] //for the dropdown
+       concentration: [],
+       departmentalInterests: [],
+       departments: [] //for the dropdowns
      };
      this.displayExistingPreferences();
      this.loadDepartments();
@@ -20,43 +48,52 @@ export default class PreferencesScreen extends React.Component {
   render() {
 
     let classYearField = (
-      <div className="classYear">
-        <select name="classYear" 
-            value={this.state.classYear} 
-            onChange={ (e) => {this.setState({classYear: e.target.value})} }> 
-          <option value="Freshman">Freshman</option>
-          <option value="Sophomore">Sophomore</option>
-          <option value="Junior">Junior</option>
-          <option value="Senior">Senior</option>
-          <option value="Graduate Student">Grad</option>
-        </select>
-      </div>
+      <select name="classYear" 
+          value={this.state.classYear}
+          onChange={ (e) => {this.setState({classYear: e.target.value})} }> 
+        <option value="Freshman">Freshman</option>
+        <option value="Sophomore">Sophomore</option>
+        <option value="Junior">Junior</option>
+        <option value="Senior">Senior</option>
+        <option value="Graduate Student">Grad</option>
+      </select>
     );
 
-    let concentrationField = (
-      <div className="concentration">
-        <select name="concentration" 
-            value={this.state.concentration} 
-            onChange={ (e) => {this.setState({concentration: e.target.value})} }> 
-          {this.state.departments}
-        </select>
+    // janky
+
+    let concentrationFields = (
+      <div className="concentrationFields">
+      <ConcentrationField 
+        index='0' 
+        concentrations={this.state.concentration}
+        departments={this.state.departments} 
+        onchange={this.updateConcentration.bind(this)}/>
+      <ConcentrationField 
+        index='1' 
+        concentrations={this.state.concentration}
+        departments={this.state.departments} 
+        onchange={this.updateConcentration.bind(this)}/>
+      <ConcentrationField 
+        index='2' 
+        concentrations={this.state.concentration}
+        departments={this.state.departments} 
+        onchange={this.updateConcentration.bind(this)}/>
       </div>
-    );
+    )
 
     let departmentalInterestsField = (
-      <div className="departmentalInterests">
-        <select name="departmentalInterests" 
-            value={this.state.departmentalInterests} 
-            onChange={ (e) => {
-              this.setState({departmentalInterests: e.target.value})
-            } }> 
-          {this.state.departments}
-        </select>
-        <div className='addMoreInterests'>+</div>
-      </div>
+      <select name="departmentalInterests" 
+          value={this.state.departmentalInterests[0]} 
+          onChange={ (e) => {
+            this.setState({departmentalInterests: [e.target.value]})
+          } }>
+        {this.state.departments}
+      </select>
     );
 
     let doneButton = <a href='#' onClick={() => this.done()}>done</a>;
+    let addConcentrationButton = <div className='addMore' onClick={() => this.updateConcentration('', this.state.concentration.length+1)} >+</div>;
+    let addInterestButton = <div className='addMore'>+</div>;
 
     return (
       <div className='preferences'>
@@ -64,17 +101,21 @@ export default class PreferencesScreen extends React.Component {
         <div className="line"></div>
         <div className="prefSection">
           <label>Class Year</label>
-            {classYearField}
+          {classYearField}
         </div>
         <div className="line"></div>
         <div className="prefSection">
           <label>Concentration</label>
-            {concentrationField}
+          {concentrationFields}
+          {addConcentrationButton}
         </div>
         <div className="line"></div>
         <div className="prefSection">
           <label>Departmental Interests</label>
-            {departmentalInterestsField}
+          {departmentalInterestsField}
+          {departmentalInterestsField}
+          {departmentalInterestsField}
+          {addInterestButton}
         </div>
         <div className="prefSection">
           {doneButton}
@@ -98,9 +139,10 @@ export default class PreferencesScreen extends React.Component {
   post() {
     let prefs = {
       year: this.state.classYear,
-      concentration: this.state.concentration,
-      interests: this.state.departmentalInterests
+      concentration: this.state.concentration.join(','),
+      interests: this.state.departmentalInterests.join(',')
     };
+    console.log(prefs);
     api.postPrefs(prefs);
   }
 
@@ -111,8 +153,8 @@ export default class PreferencesScreen extends React.Component {
   displayExistingPreferences() {
     let display = (prefs) => {
       this.setState({classYear: prefs.class_year==undefined ? '' : prefs.class_year});
-      this.setState({concentration: prefs.concentration==undefined ? '' : prefs.concentration});
-      this.setState({departmentalInterests: prefs.dept_interests==undefined ? '' : prefs.dept_interests});
+      this.setState({concentration: prefs.concentration==undefined ? [] : prefs.concentration});
+      this.setState({departmentalInterests: prefs.dept_interests==undefined ? [] : prefs.dept_interests});
     }
     api.getPrefs(display);
   }
@@ -127,6 +169,13 @@ export default class PreferencesScreen extends React.Component {
       })});
     };
     api.getDepartments(departmentDropdown);
+  }
+
+  updateConcentration(newConc, index) {
+    let newConcentrationArray = this.state.concentration;
+    newConcentrationArray[index] = newConc;
+    this.setState({concentration: newConcentrationArray});
+    console.log(this.state.concentration);
   }
 
 }
