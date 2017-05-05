5a0281a6cb921a54431f38281bc2ce9f42cc583d
kokonakajima <koko_nakajima@brown.edu>
2017-05-05 17:09:39 -0400

    setup for timeslots

--- frontend/calendarDayView.jsx
+++ frontend/calendarDayView.jsx
@@ -4,20 +4,42 @@ import api from './api.jsx';
 import CalendarSectionObject from './calendarSectionObject.jsx';
 
 export default class CalendarDayView extends React.Component {
+	
 	constructor(props) {
 		super(props);
 		this.state = {
+			startTimes: []
 		};
 	}
+
+	componentDidUpdate() {
+		if (this.props.sections) {
+			this.props.sections.map((section) => {
+				let currTimes = this.state.startTimes;
+				if (!currTimes.includes(section.props.start)) {
+					this.setState({startTimes: currTimes.concat([section.props.start]).sort(this.orderTimes)});
+				};
+			});
+		};
+	}
+
+	orderTimes = (s1, s2) => {
+		return (s1 - s2);
+	};
+
 
 	render() {
 		let day = this.props.day;
 		let sections = this.props.sections;
 		let style= (this.props.style) ? this.props.style : {};
 
+		console.log(this.state.startTimes);
+
+
 		return (
 			<div className='calendarDayView' style={style}>
 				<h3 className='dayLabel'>{day}</h3>
+				<h4 className='timeLabel'>Time</h4>
 				<div className='courses'>
 					{sections}
 				</div>
