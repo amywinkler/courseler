79aa16820b1eb62c252018daf079501cc0623314
= <nateparro2t@gmail.com>
2017-05-06 15:50:22 -0400

    restyle share screen a bit

--- frontend/shareSheet.jsx
+++ frontend/shareSheet.jsx
@@ -13,7 +13,7 @@ export default class ShareSheet extends React.Component {
   render() {
     return (
       <div className='ShareSheet'>
-        <h1>Share your Cart</h1>
+        <h2>Share your Cart</h2>
         <p>Send this link to friends, and they'll be able to see which classes you've got in your cart. As you add or remove classes, they'll see the most up-to-date version.</p>
         <input onClick={(e) => e.target.setSelectionRange(0, e.target.value.length)} value={this.state.url || ''} />
       </div>
