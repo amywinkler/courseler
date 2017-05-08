65cc1b7c0b7836715b52273d88ac2dd46e5a6354
kokonakajima <koko_nakajima@brown.edu>
2017-05-08 01:45:06 -0400

    color2

--- frontend/courseColor.jsx
+++ frontend/courseColor.jsx
@@ -4,7 +4,7 @@ export let getDepartmentColor = (department) => {
   // console.log(department);
 
   let stem = new Set(["Applied Mathematics","Biology","Chemistry","Computer Science","Engineering","Environmental Science","Geology","Mathematics","Medical Education","BioMed-Neuroscience","Public Health","Physics","Program in Liberal Medical Education"]);
-  let humanities = new Set(["Classics","Comparative Literature","Contemplative Studies","Education","English","Gender and Sexuality Studies","History of Art and Architecture","Humanities","Music","Philosophy","Religious Studies","Theatre Arts and Performance Studies","Visual Art"]);
+  let humanities = new Set(["Classics","Comparative Literature","Contemplative Studies","Education","English","Gender and Sexuality Studies","Literary Arts", "History of Art and Architecture","Humanities","Music","Philosophy","Religious Studies","Theatre Arts and Performance Studies","Visual Art"]);
   let socialSciences = new Set(["Africana Studies","American Studies","Anthropology","Archeology","Business Entrepreneurship & Organizations","Cognitive Linguistic & Psychological Sciences","Development Studies","Economics","Ethnic Studies","History","International Relations","Modern Culture and Media","Public Policy","Political Science","Science and Society","Sociology","Urban Studies"]);
   let regionalStudies = new Set(["Arabic","Assyriology","Chinese","Czech","East Asian Studies","Egyptology","English for Internationals","French","Greek","German","Hispanic Studies","Hindi-Urdu","Italian","Japanese","Judaic Studies","Korean","Latin","Middle East Studies","Modern Greek","Polish","Portugese and Brazilian Studies","Persian","Russian","Sanskrit","American Sign Language","Swedish","Turkish"]);
 
@@ -39,7 +39,7 @@ export let getDepartmentColor = (department) => {
     // return "linear-gradient(135deg, rgb(243, 172, 12) 0%, rgb(173, 71, 202) 100%)";
     return "linear-gradient(135deg, rgb(121, 182, 218) 0%, rgb(56, 91, 199) 100%)";
   } else {
-    return "linear-gradient(135deg, rgb(243, 172, 12) 0%, rgb(173, 71, 202) 100%)";
+    return "linear-gradient(135deg, rgb(121, 182, 218) 0%, rgb(56, 91, 199) 100%)";
   }
 
 }
